bd3f9bad7cabe9995b1c936fe6b1047cb5a7ddfe
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-29 16:05:31 -0400

    update app

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,20 @@
 import 'package:dart_cli_demo/dart_cli_demo.dart' as dart_cli_demo;
 
+
 void main(List<String> arguments) {
-  print('Hello world: ${dart_cli_demo.calculate()}!');
+  print('---------------------------------------------');
+  print(' ');
+  print(' ');
+  print('Welcome to the DART Application Demo!');
+  print(' ');
+  print('*********************************************');
+  print(' ');
+  print('Here are some demo applications wrapped within');
+  print('a dart app, this will allow you to see how they run');
+  print('and some examples of dart coding');
+  print(' ');
+  print('Calling the Math Module');
+  print(' ');
+  print('Math Says: ${dart_cli_demo.calculate()}!');
 }
+
