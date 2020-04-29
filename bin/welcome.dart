08540d68e1d8e33eeaa5f3c4c4e30a5a4bc273d5
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-29 15:31:05 -0400

    upate the application harness

---
+++ bin/welcome.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('---------------------------------------------');
+  print(' ');
+  print(' ');
+  print('Welcome to the DART Application Demo!');
+  print(' ');
+  print('*********************************************');
+  print(' ');
+  print(' ');
+  print(' ');
+  print(' ');
+  print(' ');
+  print(' ');
+  print(' ');
+}
