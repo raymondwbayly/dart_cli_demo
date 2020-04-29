eb82f69ce2ce653c1262d669eee6ff4af478a31c
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-29 15:37:37 -0400

    update the dart CLI demo

--- bin/welcome.dart
+++ bin/welcome.dart
@@ -6,9 +6,9 @@ void main() {
   print(' ');
   print('*********************************************');
   print(' ');
-  print(' ');
-  print(' ');
-  print(' ');
+  print('Here are some demo applications wrapped within');
+  print('a dart app, this will allow you to see how they run');
+  print('and some examples of dart coding');
   print(' ');
   print(' ');
   print(' ');
